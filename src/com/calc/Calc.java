fd9a3e439ac4b40d81ee8c3a7086fa23538c59e7
SiriChandana1234 <21d41a05r3@gmail.com>
2025-11-15 19:34:09 +0530

    THIS IS INITIAL COMMIT

---
+++ src/com/calc/Calc.java
@@ -0,0 +1,8 @@
+package com.calc;
+
+public interface Calc {
+     public int add(int a, int b);
+     public double sub( int a, int b);
+     public int mul(int a, int b);
+     public double dev( int a, int b);
+}
