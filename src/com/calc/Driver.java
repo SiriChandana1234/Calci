d6cfc7f204e6097da38bbcce269209647dd5c84f
SiriChandana1234 <21d41a05r3@gmail.com>
2025-11-18 19:13:51 +0530

    driver class created

---
+++ src/com/calc/Driver.java
@@ -0,0 +1,37 @@
+package com.calc;
+import java.util.Scanner;
+public class Driver {
+   public static void main(String[] args) {
+	 Scanner sc=new Scanner(System.in);
+	 CalciImplementation c=new CalciImplementation();
+	 
+	 System.out.println("Simple Calculator");
+     System.out.print("Enter first number: ");
+     double a = sc.nextDouble();
+
+     System.out.print("Enter second number: ");
+     double b = sc.nextDouble();
+
+     System.out.println("Choose operation (+, -, *, /): ");
+     char operation = sc.next().charAt(0);
+     
+     switch(operation) {
+     
+     case '+':
+         
+               break;
+     case '-':
+         
+               break;
+     case '*':
+        
+               break;
+     case '/':
+         
+               break;
+     default:
+         System.out.println("Invalid operation.");
+     }
+
+}
+}
