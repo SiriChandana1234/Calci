5a143ea8f42e64db905969d480b41ab98e86597c
SiriChandana1234 <21d41a05r3@gmail.com>
2025-11-18 17:40:53 +0530

    message

---
+++ src/com/calc/CalciImplementation.java
@@ -0,0 +1,29 @@
+package com.calc;
+
+public class CalciImplementation  implements Calc{
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int mul(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double dev(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
